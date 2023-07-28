0327b112438b728513967163ef8a1bdc8de2f3eb
literallynotbruh <138126386+literallynotbruh@users.noreply.github.com>
2023-07-28 14:38:11 +0600

    Update Seek POV.lua
    Fixed When Camera is black(again)

--- Seek POV.lua
+++ Seek POV.lua
@@ -3,4 +3,6 @@ local RunService = game:GetService("RunService")
 RunService.RenderStepped:Connect(function()
     workspace.Camera.CFrame = workspace.SeekMoving.SeekRig.Head.CFrame
     workspace.SeekMoving.SeekRig.Head.Transparency = 1
+    workspace.SeekMoving.SeekRig.Head.Black:Destroy()
+    workspace.SeekMoving.SeekRig.Head.Eye:Destroy()
 end)
